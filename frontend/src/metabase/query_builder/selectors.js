57a6e72a5669f3d59ecde868ef0c9ad942cbb474
Dalton Johnson <daltojohnso@users.noreply.github.com>
2021-06-30 15:41:01 -0700

    add entity menu for model state + associated action

--- frontend/src/metabase/query_builder/selectors.js
+++ frontend/src/metabase/query_builder/selectors.js
@@ -456,3 +456,8 @@ export const getIsLiveResizable = createSelector(
     }
   },
 );
+
+export const getIsModel = createSelector(
+  [getQuestion],
+  question => question && (question.displayName() || "").startsWith("MODEL: "),
+);
